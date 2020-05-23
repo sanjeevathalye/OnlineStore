c454151c5f246294651e3f7ee7c8564151311b87
sanjeevathalye <sanjeevathalye@yahoo.com>
2020-05-23 15:38:19 -0400

    Implemented order module

--- src/Model/model.module.ts
+++ src/Model/model.module.ts
@@ -1,10 +1,12 @@
+import { OrderRepository } from './order.repository';
 import { Cart } from './../app/store/cart.model';
 import { NgModule } from '@angular/core';
 import { ProductRepository } from './product.repository';
 import { StaticDataSource } from './static.datasource' ;
+import { Order } from './order.model';
 
 @NgModule ({
-    providers: [ ProductRepository, StaticDataSource, Cart]
+    providers: [ ProductRepository, StaticDataSource, Cart, Order, OrderRepository]
 })
 
 export class ModelModule {
--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,14 +1,31 @@
+import { OrderRepository } from './../../Model/order.repository';
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/Model/order.model';
+import { Subscriber } from 'rxjs';
 
 @Component({
-  templateUrl: './checkout.component.html'
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
 })
 
 export class CheckoutComponent implements OnInit {
+  orderSent = false;
+  orderSubmitted = false;
 
-  constructor() { }
+  constructor( public order: Order, public repo: OrderRepository) { }
 
   ngOnInit(): void {
+  }
+
+  submitOrder (form: NgForm) {
+    this.orderSubmitted = true;
+    if (form.valid) {
+      this.repo.saveOrder(this.order).subscribe (order => {
+        this.order.clearOrder();
+        this.orderSent = true;
+        this.orderSubmitted = false;
+      })
+    }
   }
 
 }
