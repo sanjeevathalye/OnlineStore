502e12c87e9d8e712d6a0adf2087da94b7189031
sanjeevathalye <sanjeevathalye@yahoo.com>
2020-06-02 17:01:54 -0400

    Added admin functions to product.repository and order.repository classes

--- src/Model/order.repository.ts
+++ src/Model/order.repository.ts
@@ -8,18 +8,40 @@ import { Order } from './order.model';
 
 export class OrderRepository {
 
-    private order: Order[] = [] ;
+    private orders: Order[] = [] ;
+    private loaded = false;
 
     constructor(private ds: RestDataSource) {
 
     }
 
+    loadOrders() {
+        this.loaded = true;
+        this.ds.getOrders().subscribe( orders => this.orders = orders);
+    }
+
     getOrders(): Order[] {
-        return this.order ;
+        return this.orders ;
     }
     
     saveOrder( order: Order): Observable<Order> {
         return this.ds.saveOrder(order);
+    }
+
+    // Admin funcitons
+
+    updateOrder(order: Order) {
+        this.ds.updateOrder(order).subscribe(order =>
+            {
+                this.orders.splice(this.orders.findIndex(o => o.orderID == order.orderID), 1, order);
+            });
+    }
+
+    deleteOrder(id: number) {
+        this.ds.deleteOrder(id).subscribe(o =>
+            {
+                this.orders.splice(this.orders.findIndex(o => o.orderID == id), 1);
+            });
     }
 
 }
