c1fc027da204aef85612e3b962d5da8aecd083ed
sanjeevathalye <sanjeevathalye@yahoo.com>
2020-06-02 22:09:59 -0400

    Created components for admin functionality, updated admin view

--- src/app/Admin/admin.module.ts
+++ src/app/Admin/admin.module.ts
@@ -1,3 +1,7 @@
+import { AuthGuard } from './auth.guard';
+import { OrderTableComponent } from './orderTable.component';
+import { ProductEditorComponent } from './productEditor.component';
+import { ProductTableComponent } from './productTable.component';
 
 import { AdminComponent } from './admin.component';
 import { AuthComponent } from './auth.component';
@@ -8,13 +12,21 @@ import { CommonModule } from '@angular/common';
 
 let routing = RouterModule.forChild([
         { path: 'auth', component: AuthComponent } ,
-        { path: 'main', component: AdminComponent },
+        { path: 'main', component: AdminComponent, 
+            children:[
+                { path: 'products/:mode/:id',   component:ProductEditorComponent},
+                { path: 'products/:mode',       component:ProductEditorComponent},
+                { path: 'products',             component:ProductTableComponent},
+                { path: 'orders',               component:OrderTableComponent},
+                { path: '**',                   redirectTo: 'products' }
+            ]
+        },
         { path: '**', redirectTo: 'auth'}
     ]);
 
 @NgModule ({
     imports: [CommonModule, routing, FormsModule],
-    declarations: [AuthComponent, AdminComponent]
+    declarations: [AuthComponent, AdminComponent, ProductEditorComponent, ProductTableComponent, OrderTableComponent]
 })
 
 export class AdminModule {
---
+++ src/app/Admin/productEditor.component.ts
@@ -0,0 +1,11 @@
+// This component is responsible for allowing the dministrator to enter the details required to create or edit a product
+
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './productEditor.component.html'
+})
+
+export class ProductEditorComponent {
+
+}
