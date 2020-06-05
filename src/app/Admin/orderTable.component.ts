74d19c8d7ed0e7b9ccf796ddfdc78cdd85a505b5
sanjeevathalye <sanjeevathalye@yahoo.com>
2020-06-05 12:42:11 -0400

    Admin order functionality

--- src/app/Admin/orderTable.component.ts
+++ src/app/Admin/orderTable.component.ts
@@ -1,11 +1,37 @@
+
 // This component is responsible for showing the list of oders
 
+import { OrderRepository } from './../../Model/order.repository';
+import { Order } from 'src/Model/order.model';
 import { Component } from '@angular/core';
+
 
 @Component({
   templateUrl: './orderTable.component.html'
 })
 
 export class OrderTableComponent {
+
+  shipped = false;
+
+  constructor(private repository: OrderRepository) {
+
+  }
+
+  // get unshipped orders
+  getOrders() : Order[] {
+    return this.repository.getOrders().filter(o => !o.shipped || this.shipped);
+  }
+
+  // Mark the order in respository as shipped 
+  markShipped(order: Order) {
+    order.shipped = true;
+    this.repository.updateOrder(order);
+  }
+
+  // Delete order from the respository
+  deleteOrder(id: number) {
+    this.repository.deleteOrder(id);
+  }
 
 }
